6af90d6cb1e2e61fa23897c9c8dcd6c538c93b47
Anish Dharam <anish_dharam@brown.edu>
2025-06-05 14:48:52 -0400

    basic outline

---
+++ main.c
@@ -0,0 +1,37 @@
+#define EXIT_SUCCESS 1
+
+
+int main(int argc, char** argv) {
+    //config
+
+    //loop
+    sh_loop();
+
+    //shutdown
+    return EXIT_SUCCESS;
+}
+
+int sh_loop() {
+    int status;
+    while (status) {
+        char* line = read_line();
+        char** args = get_args(line);
+        status = execute(args);
+    }
+
+    return status;
+}
+
+
+char* read_line() {
+    return "";
+};
+
+char** get_args(char* line) {
+    char** res;
+    return res;
+}
+
+int execute(char** args) {
+    return 0;
+}
