a98160e4a9f0f54da9fc94288ef1f44090e93f21
Anish Dharam <anish_dharam@brown.edu>
2025-07-08 18:14:36 -0400

    added builtin funcs

--- builtins.h
+++ builtins.h
@@ -4,5 +4,11 @@ int help(char** args);
 int exit(char** args);
 
 char* builtin_funcs[] = {
-    &
+    &cd,
+    &help,
+    &exit
+};
+
+int lsh_num_builtins() {
+    return sizeof(builtin_str) / sizeof(char *);
 }
